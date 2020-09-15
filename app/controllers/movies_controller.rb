979abecece892cd779893d6249e4a222ab4140cc
EC2 Default User <ec2-user@ip-172-31-7-175.us-east-2.compute.internal>
2020-09-15 01:49:46 +0000

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,16 @@
 class MoviesController < ApplicationController
-
+  
   def movie_params
     params.require(:movie).permit(:title, :rating, :description, :release_date)
   end
-
+  
+  
+  def initialize 
+    @all_ratings = Movie.all_ratings
+    super
+  end
+  
+  
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
@@ -11,7 +18,42 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    redirect = false
+    
+     if params[:sort]
+            @sorting = params[:sort]
+        elsif session[:sort]
+            @sorting = session[:sort]
+            redirect = true
+     end
+    
+     if params[:ratings]
+            @ratings = params[:ratings]
+        elsif session[:ratings]
+            @ratings = session[:ratings]
+            redirect = true
+        else
+            @all_ratings.each do |rat|
+                (@ratings ||= { })[rat] = 1
+            end
+            redirect = true
+     end
+
+        if redirect
+            redirect_to movies_path(:sort => @sorting, :ratings => @ratings)
+        end
+
+        Movie.find(:all, :order => @sorting ? @sorting : :id).each do |mv|
+            if @ratings.keys.include? mv[:rating]
+                (@movies ||= [ ]) << mv
+            end
+        end
+
+        session[:sort]    = @sorting
+        session[:ratings] = @ratings
+  
+        
+   # @movies = Movie.all
   end
 
   def new
