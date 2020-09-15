2904b9eca9e19eb149d2e55528975942e002569e
EC2 Default User <ec2-user@ip-172-31-7-175.us-east-2.compute.internal>
2020-09-15 02:30:58 +0000

    update

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,9 @@
 class MoviesController < ApplicationController
-  
+
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :description, :release_date)
+  end
+
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
@@ -7,44 +11,7 @@ class MoviesController < ApplicationController
   end
 
   def index
-    #movie ratings
-    @all_ratings = Movie.get_possible_ratings
-    
-    #update ratings
-    unless params[:ratings].nil?
-      @filtered_ratings = params[:ratings]
-      session[:filtered_ratings]=@filtered_ratings
-    end
-      
-    #if user has a sorting
-    if params[:sorting].nil?
-    else
-      session[:sorting] = params[:sorting]
-    end
-    
-    if params[:sorting].nil?&& params[:ratings].nil? && session[:filtered_ratings]
-      @filtered_ratings = session[:filtered_ratings]
-      @sorting = session[:sorting]
-      flash.keep 
-      redirect_to movies_path({order_by:@sorting, ratings:@filtered_ratings})
-    end
-    
     @movies = Movie.all
-    
-    #filter movies based on rating
-    if session[:filtered_ratings]
-      @movies = @movies.select { |movie| session[:filtered_ratings].include? movie.rating}
-    end 
-    #title sort 
-    if session[:sorting] == "title"
-      @movies = @movies.sort!{ |a,b| a.title <=> b.title}
-      @movie_highlight = "hilite"
-    elsif session[:sorting] == "release_date"
-      @movies = @movies.sort!{|a,b| a.release_date <=> b.release_date}
-      @date_highlight = "hilite"
-    end
-    
-      
   end
 
   def new
@@ -74,5 +41,5 @@ class MoviesController < ApplicationController
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
   end
- 
+
 end
